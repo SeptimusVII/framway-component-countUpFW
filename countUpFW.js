d5ac6d7ea2a53bf1d4d7bf24dd864d79d74ca0f4
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-05-23 15:43:37 +0200

    Feat: add prefix, separator and decimal parameters

--- countUpFW.js
+++ countUpFW.js
@@ -3,8 +3,8 @@ module.exports = function(app){
     var CountUpFW = Object.getPrototypeOf(app).CountUpFW = new app.Component("countUpFW");
     // CountUpFW.debug = true;
     CountUpFW.createdAt      = "2.0.0";
-    CountUpFW.lastUpdate     = "2.0.0";
-    CountUpFW.version        = "1";
+    CountUpFW.lastUpdate     = "2.3.0";
+    CountUpFW.version        = "1.1.0";
     // CountUpFW.factoryExclude = true;
     // CountUpFW.loadingMsg     = "This message will display in the console when component will be loaded.";
     // CountUpFW.requires       = [];
@@ -16,12 +16,16 @@ module.exports = function(app){
         countUp.decimals = (countUp.decimals !== undefined) ? countUp.decimals : countUp.getData('decimals',0);
         countUp.duration = (countUp.duration !== undefined) ? countUp.duration : countUp.getData('duration',2);
         countUp.delay = (countUp.delay !== undefined) ? countUp.delay : countUp.getData('delay',0);
+        countUp.prefix = (countUp.prefix !== undefined) ? countUp.prefix : countUp.getData('prefix','');
+        countUp.separator = (countUp.separator !== undefined) ? countUp.separator : countUp.getData('separator',true);
+        countUp.decimal = (countUp.decimal !== undefined) ? countUp.decimal : countUp.getData('decimal','.');
         countUp.target = (countUp.$el.find('.countUpFW__num').length) ? countUp.$el.find('.countUpFW__num').get(0) : countUp.$el.get(0);
         countUp.options = {
             useEasing: true,
             useGrouping: true,
-            separator: '',
-            decimal: '.',
+            separator: countUp.separator?' ':'',
+            decimal: countUp.decimal,
+            prefix: countUp.prefix,
             enableScrollSpy: true,
             scrollSpyDelay: countUp.delay*1000,
             startVal: countUp.startVal,
